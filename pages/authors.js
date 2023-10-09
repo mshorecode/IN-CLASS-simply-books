29a86eefa3295c730b9094c272e4a1373d1b6f98
Ryan Shore <mshorecode@gmail.com>
2023-10-09 01:07:44 -0500

    setup authors, changed home to welcome screen, moved books

---
+++ pages/authors.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { Button } from 'react-bootstrap';
+import { useAuth } from '../utils/context/authContext';
+import { getAuthors } from '../api/authorData';
+import AuthorCard from '../components/AuthorCard';
+
+export default function ShowBooks() {
+  // TODO: Set a state for books
+  const [author, setAuthor] = useState([]);
+
+  // TODO: Get user ID using useAuth Hook
+  const { user } = useAuth();
+
+  // TODO: create a function that makes the API call to get all the books
+  const getAllTheAuthors = () => {
+    getAuthors(user.uid).then(setAuthor);
+  };
+
+  // TODO: make the call to the API to get all the books on component render
+  useEffect(() => {
+    getAllTheAuthors();
+  }, []); // TODO: Ask what this linter error means
+
+  return (
+    <div className="text-center my-4">
+      <Link href="/book/new" passHref>
+        <Button>Add An Author</Button>
+      </Link>
+      <div className="d-flex flex-wrap">
+        {/* TODO: map over books here using BookCard component */}
+        {author.map((authors) => (
+          <AuthorCard key={authors.firebaseKey} authorObj={authors} onUpdate={getAllTheAuthors} />
+        ))}
+      </div>
+
+    </div>
+  );
+}
